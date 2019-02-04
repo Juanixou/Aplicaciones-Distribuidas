c0f8339027935a32709fb9907b39a3e717d1f1d9
Juanixou <juansilvamora@gmail.com>
2019-02-04 12:36:09 +0100

    Configuración del Proyecto.
    El proyecto está configurado para usar mustache y comenzar a funcionar.

---
+++ URJCGamingNews/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/prueba")
+	 public String greeting(Model model) {
+	 model.addAttribute("name", "URJC Gaming News");
+	 /*
+	 model.addAttribute("css","/URJCGamingNews/src/main/resources/templates/layout/styles/layout.css");
+	 model.addAttribute("kh3","/URJCGamingNews/src/main/resources/templates/images/kh3.jpg");
+	 model.addAttribute("jquerymin","/URJCGamingNews/src/main/resources/templates/layout/scripts/jquery.min.js");
+	 model.addAttribute("jquerybacktotop","/URJCGamingNews/src/main/resources/templates/layout/scripts/jquery.backtotop.js");
+	 model.addAttribute("jquerymobile","/URJCGamingNews/src/main/resources/templates/layout/scripts/jquery.mobilemenu.js");
+	 model.addAttribute("slider","/URJCGamingNews/src/main/resources/templates/layout/scripts/slider.js");
+	 */
+	 return "greeting_template";
+	 }
+
+}
