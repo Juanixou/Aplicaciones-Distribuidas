1bbcb899bff22a457f6c374933b59a7c510aed2c
jpguirado <jp.guirado@alumnos.urjc.es>
2019-02-05 14:35:42 +0100

    Template con Mustache.
    Se ha introducido el template como mustache, y yaestá operativo.

--- URJCGamingNews/src/main/java/com/example/demo/GreetingController.java
+++ URJCGamingNews/src/main/java/com/example/demo/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GreetingController {
 
+	
+	
 	@GetMapping("/prueba")
 	 public String greeting(Model model) {
 	 model.addAttribute("name", "URJC Gaming News");
-	 /*
+	 model.addAttribute("resources", "/resources");
+/*	 
 	 model.addAttribute("css","/URJCGamingNews/src/main/resources/templates/layout/styles/layout.css");
 	 model.addAttribute("kh3","/URJCGamingNews/src/main/resources/templates/images/kh3.jpg");
 	 model.addAttribute("jquerymin","/URJCGamingNews/src/main/resources/templates/layout/scripts/jquery.min.js");
 	 model.addAttribute("jquerybacktotop","/URJCGamingNews/src/main/resources/templates/layout/scripts/jquery.backtotop.js");
 	 model.addAttribute("jquerymobile","/URJCGamingNews/src/main/resources/templates/layout/scripts/jquery.mobilemenu.js");
 	 model.addAttribute("slider","/URJCGamingNews/src/main/resources/templates/layout/scripts/slider.js");
-	 */
-	 return "greeting_template";
+*/	 
+	 return "index";
 	 }
 
 }
