7eb0450a98b4edc5ecf264262ff5d22865328115
Angel Luis Serrano <al.serranog@alumnos.urjc.es>
2019-02-14 12:52:30 +0100

    Seccion de añadir noticia casi terminada.

---
+++ URJCGamingNews/src/main/java/com/example/demo/controladorEscribirNoticia.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class controladorEscribirNoticia {
+	
+
+	@GetMapping("/escribirNoticia")
+	public String escribirNoticia (Model mod3el) {
+		
+		return "escribirNoticia";
+	}
+	
+	
+	
+	@PostMapping("/checkNew")
+	public String checkNew(Model model, @RequestParam String uname, @RequestParam String titulo,
+			String subirImagen,@RequestParam String textoNoticia, @RequestParam String descripcion) {
+
+		model.addAttribute("uname", uname);
+		model.addAttribute("titulo", titulo);
+		model.addAttribute("descripcion", descripcion);
+		model.addAttribute("subirImagen",subirImagen);
+		model.addAttribute("textoNoticia", textoNoticia);
+		
+		
+		LocalDate localDate = LocalDate.now();
+		 
+		Articulo articulo = new Articulo(titulo,descripcion,textoNoticia,
+					"RDR2.png",localDate.toString(),uname);
+			
+		
+
+		return "checkNew";
+	}
+
+}
