580f3900f452a5a8ee2e33c6b74c04ebcc303060
Juanixou <juansilvamora@gmail.com>
2019-03-11 18:52:10 +0100

    Cambios del newsletter.
    Se han añadido clases de Newsletter como repositorio y BD.

---
+++ URJCGamingNews/src/main/java/com/example/demo/ControladorNewsletter.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ControladorNewsletter {
+
+	@Autowired
+	 private  NewsletterRepository repositorioNewsletter;
+	
+	
+	/*
+	 * Devuelve el formulario de login 
+	 */
+	@GetMapping("/newsletter")
+	public String login(Model model) {
+		return "newsletter";
+	}
+	
+	@PostMapping("/checkNewsletter")
+	public String checkNewsletter(Model model, @RequestParam String umail) {
+		
+		
+		List<Newsletter> listaEmail = repositorioNewsletter.findByEmail(umail);
+		Newsletter mail = listaEmail.get(0);
+		
+		if(!mail.equals("")) {
+			repositorioNewsletter.save(new Newsletter(umail));
+			return "checkNewsletter";
+		}else {
+			return "inicio";
+		}
+	}
+	
+	
+}
--- URJCGamingNews/src/main/java/com/example/demo/NewsletterRepository.java
+++ URJCGamingNews/src/main/java/com/example/demo/NewsletterRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsletterRepository extends JpaRepository<Newsletter, Long>{
 
+	List<Newsletter> findByEmail(String email);
+	
 }
