9da0cce23662abc9deaeb7a8eb7d9714852ef228
Juanixou <juansilvamora@gmail.com>
2019-04-23 10:32:40 +0200

    Solución de errores.
    Se han solucionado errores de la BD y actualizado el jar.

--- URJCGamingNews/src/main/java/com/example/demo/SecurityConfiguration.java
+++ URJCGamingNews/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -34,11 +34,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		http.authorizeRequests().antMatchers("/escribirNoticia").hasAnyRole("ADMIN");
 		http.authorizeRequests().antMatchers("/checkNew").hasAnyRole("ADMIN");
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/checklogin").hasAnyRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "https://127.0.0.1:8443/checklogin").hasAnyRole("USER");
 		
 		//Login
 		 http.formLogin().loginPage("/login");
-		 http.formLogin().loginProcessingUrl("/checklogin");
+		 http.formLogin().loginProcessingUrl("https://127.0.0.1:8443/checklogin");
 		 http.formLogin().usernameParameter("uname");
 		 http.formLogin().passwordParameter("psw");
 		 http.formLogin().defaultSuccessUrl("/");
